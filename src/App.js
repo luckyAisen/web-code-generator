d0896d4261a3f30d24cc5a6cfea157f39cf646d5
Aisen60 <caixusheng60@gmail.com>
2021-01-26 14:09:39 +0800

    refactor: init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CodePage from "./views/code-page";
+function App() {
+  return (
+    <div className="app-wrapper">
+      <CodePage></CodePage>
+    </div>
+  );
+}
+
+export default App;
